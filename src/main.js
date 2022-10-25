9bec62d61d55a1bad2ec9f282cb302bc133e8156
Julie Jakobsen <juliehage@live.dk>
2022-10-25 16:48:04 +0200

    second commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
+// Import af AlpineJS
+import Alpine from "alpinejs";
+window.Alpine = Alpine;
+Alpine.start();
+
 // dark/light mode
 const btnLight = document.querySelector("#light");
 const btnDark = document.querySelector("#dark");
@@ -37,6 +42,6 @@ inView(".hjerterinview", ({ target }) => {
   animate(
     target.querySelectorAll(".hjerte"),
     { x: [2000, 0] },
-    { duration: 1, delay: stagger(1, { start: 0.25 }) }
+    { duration: 0.75, delay: stagger(1, { start: 0.25 }) }
   );
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,11 +6,25 @@ module.exports = {
     colors: {
       yellow: "#EEBB22",
       white: "#FFFAFA",
-      black: "#151515",
-      grey: "#333333",
+      gray: "#333333",
+      black: "#000000",
       red: "#ff6c00",
+      darkred: "#C44500",
+      lightgray: "#B5B5B6",
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        bg_web: "url('img/bg_web.png')",
+        bg_light_web: "url('img/bg_light_web.png')",
+      },
+      inset: {
+        bjaelke_position: "38rem",
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
 };
